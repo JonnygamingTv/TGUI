b9d2e8997adcbab1ac48893d0c36286fdb7e1f70
Jonnygaming Tv <22498524+JonnygamingTv@users.noreply.github.com>
2022-02-09 13:10:23 +0100

    Add files via upload

--- GUI_SFML_Template/SFML_Template/Btn.cpp
+++ GUI_SFML_Template/SFML_Template/Btn.cpp
@@ -11,9 +11,22 @@ tgui::Button::Ptr Btn::get()
 	return this->button;
 }
 
+void Btn::setColor(sf::Color background, sf::Color background_hover)
+{
+	this->button->getRenderer()->setBackgroundColor(background);
+	this->button->getRenderer()->setBackgroundColorHover(background_hover);
+}
+
 void Btn::init(int x, int y,int yl, int yh, std::string cont)
 {
 	this->button->setSize(yl,yh);
 	this->button->setPosition(x, y);
 	this->button->setText(cont);
+
+
+	/* NOT USED ---- Test code
+	tgui::RendererData data = tgui::RendererData();
+	std::shared_ptr<tgui::RendererData> g = std::shared_ptr<tgui::RendererData>();
+	std::shared_ptr<tgui::RendererData> gg = g->create();
+	this->button->setRenderer(g);*/
 }
--- GUI_SFML_Template/SFML_Template/Btn.h
+++ GUI_SFML_Template/SFML_Template/Btn.h
@@ -5,6 +5,7 @@ class Btn
 public:
 	Btn(int x = 0,int y=0,int yLength=50,int yHeight=10, std::string cont="JonHosting");
 	tgui::Button::Ptr get();
+	void setColor(sf::Color background,sf::Color background_hover);
 private:
 	void init(int x, int y,int yl,int yh, std::string cont);
 	// variables
